1768ad38b45ff15a90893440d237a20ec5c064fb
crazysadboy <54902146+CrazyFatBoi2005@users.noreply.github.com>
2025-05-05 12:59:36 +0300

    init

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+export default function Index() {
+  return (
+    <Layout>
+      <div>
+        <h1>Hello everyone!</h1>
+      </div>
+    </Layout>
+  );
+}
